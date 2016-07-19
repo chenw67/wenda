f43c88bc3cae02890448d1dc5f4ab0e31c4a3c0a
chenw67 <630557748@qq.com>
2016-07-19 11:27:03 +0800

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.print("hello nowcoder");
+        System.out.print("hello");
 	// write your code here
     }
 }
